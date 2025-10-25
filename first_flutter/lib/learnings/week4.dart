9eadf3efabe84d43f4d7c1e1efb7fd0da60faa95
RBunna <bunnasovethroath@gmail.com>
2025-10-25 17:17:35 +0700

    Huge push!!

---
+++ first_flutter/lib/learnings/week4.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Week4App {
+  static MaterialApp ex1() {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Hello my name is Ronan !",
+            style: TextStyle(color: Colors.orange, fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+
+  
+}
---
+++ first_flutter/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:first_flutter/learnings/week4.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Week4App.ex1()
+  );
+}
