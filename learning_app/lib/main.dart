2d3dec064443a7f359143ef72d22dc2ebfe9b2aa
RBunna <bunnasovethroath@gmail.com>
2025-12-19 10:31:03 +0700

    Initialize navigator app

--- learning_app/lib/main.dart
+++ learning_app/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:learning_app/grocery_app/main.dart';
+// import 'package:learning_app/grocery_app/main.dart';
+import 'package:learning_app/learning_navigator/main.dart';
 
 void main() {
-  GroceryApp().main();
+  NavigatorApp().main();
 }
